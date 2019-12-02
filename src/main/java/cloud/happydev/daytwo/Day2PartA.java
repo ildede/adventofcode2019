11a9e289dd255c639b80c8aa547fb06a3e5398cd
Davide Pedron <davide@happydev.cloud>
2019-12-02 13:29:51 +0100

    Solve day 2 part A

--- src/main/java/cloud/happydev/daytwo/Day2PartA.java
+++ src/main/java/cloud/happydev/daytwo/Day2PartA.java
@@ -10,8 +10,32 @@ public class Day2PartA {
         LineReader lineReader = new LineReader();
 
         List<Integer> intcode = lineReader.getIntcode(Day2PartA.class.getResource("/day2input"));
-        System.out.println("intcode = " + intcode);
-        System.out.println("Solution day 2 part A: N.D.");
+
+        intcode.set(1, 12);
+        intcode.set(2, 2);
+        int opcodePointer = 0;
+        int opcode = intcode.get(opcodePointer);
+
+        while (opcode != 99) {
+            int first = intcode.get(intcode.get(opcodePointer+1));
+            int second = intcode.get(intcode.get(opcodePointer+2));
+            int resultPointer = intcode.get(opcodePointer+3);
+
+            switch (opcode) {
+                case 1:
+                    intcode.set(resultPointer, first + second);
+                    break;
+                case 2:
+                    intcode.set(resultPointer, first * second);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown opcode: " + opcode);
+            }
+            opcodePointer = opcodePointer + 4;
+            opcode = intcode.get(opcodePointer);
+        }
+
+        System.out.println("Solution day 2 part A: " + intcode.get(0));
     }
 
 }
