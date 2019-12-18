cc681836b1a0813607a55ce9388fb314bb69a7c5
Davide Pedron <davide@happydev.cloud>
2019-12-19 00:22:25 +0100

    Solve day 4 part B

--- src/main/java/cloud/happydev/dayfour/Day4PartB.java
+++ src/main/java/cloud/happydev/dayfour/Day4PartB.java
@@ -1,17 +1,21 @@
 package cloud.happydev.dayfour;
 
+import java.util.HashMap;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Day4PartA {
+public class Day4PartB {
 
     public static void main(String[] args) {
 
         long count = IntStream.range(153517, 630395)
                 .mapToObj(String::valueOf)
-                .filter(atLeastTwoDigitsAreTheSame())
                 .filter(theDigitsNeverDecrease())
+                .filter(atLeastTwoDigitsAreTheSame())
+                .filter(twoEqualsDigitsAreNotPartOfLargerGroup())
                 .count();
 
         System.out.println("count = " + count);
@@ -36,6 +40,23 @@ public class Day4PartA {
                 }
             }
             return false;
+        };
+    }
+
+    protected static Predicate<String> twoEqualsDigitsAreNotPartOfLargerGroup() {
+        return n -> {
+            HashMap<Character, Integer> charCountMap = new HashMap<>();
+            char[] strArray = n.toCharArray();
+            for (char c : strArray) {
+                if (charCountMap.containsKey(c)) {
+                    charCountMap.put(c, charCountMap.get(c) + 1);
+                } else {
+                    charCountMap.put(c, 1);
+                }
+            }
+            return charCountMap.values()
+                    .stream()
+                    .anyMatch(count -> count == 2);
         };
     }
 
